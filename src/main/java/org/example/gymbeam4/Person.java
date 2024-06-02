7ea6cb5399b3a3300af54921c90f96e4be2d90f7
morresmx <136925690+morresmx@users.noreply.github.com>
2024-06-02 17:26:03 +0200

    Worker Management

--- src/main/java/org/example/gymbeam4/Person.java
+++ src/main/java/org/example/gymbeam4/Person.java
@@ -11,20 +11,8 @@ public abstract class Person implements Comparable<Person> {
         this.name = name;
     }
 
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
     public String getName() {
         return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
     }
 
     @Override
